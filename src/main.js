782dc1c9ad4dec8b34afd8a4acbfd73e3e6248de
David Linner <david.linner@smarthometeam.de>
2023-05-15 08:19:53 +0200

    Update from May 10th

--- src/main.js
+++ src/main.js
@@ -1,17 +1,33 @@
 import express from 'express'
 import {promises, existsSync} from 'fs';
+import {body, validationResult} from 'express-validator';
 
 const app = express()
-
+app.use(express.static('www'));
+app.use(express.urlencoded());
 app.use(express.json());
 
+const failOnIssues = ()=>(request, response, next) => {
+    const validationIssues = validationResult(request);
+    if (!validationIssues.isEmpty()) {
+        response
+            .status(400)
+            .json(validationIssues.array());
+    } else {
+        next()
+    }
+}
+
 const DATA_FILE = 'shopping-list.json';
+
+const nameNotEmpty = () => body('name').notEmpty();
+const quantityIntMinOne = () => body('quantity').isInt({min: 1}).toInt();
 
 /**
  * Return all shopping list items as JSON.
  */
 app.get('/items', async (request, response) => {
-    const text = await promises.readFile(DATA_FILE, {encoding:'utf8'})
+    const text = await promises.readFile(DATA_FILE, {encoding: 'utf8'})
     const shoppingList = JSON.parse(text);
     response.json(shoppingList);
 })
@@ -19,41 +35,104 @@ app.get('/items', async (request, response) => {
 /**
  * Override a shopping list item at a given position.
  */
-app.put('/items/:index', async (request, response) => {
-    const data = await promises.readFile(DATA_FILE, {encoding:'utf8'})
-    const shoppingList = JSON.parse(data);
+app.put(
+    '/items/:index',
+    nameNotEmpty(),
+    quantityIntMinOne(),
+    failOnIssues(),
+    async (request, response) => {
 
-    const newItem = request.body;
-    const itemIndex = request.params['index'];
+        const data = await promises.readFile(DATA_FILE, {encoding: 'utf8'})
+        const shoppingList = JSON.parse(data);
 
-    shoppingList[itemIndex] = newItem;
+        const newItem = request.body;
+        const itemIndex = request.params['index'];
 
-    await promises.writeFile(DATA_FILE, JSON.stringify(shoppingList), {encoding:'utf-8'})
+        shoppingList[itemIndex] = newItem;
 
-    response.sendStatus(200);
-})
+        await promises.writeFile(DATA_FILE, JSON.stringify(shoppingList), {encoding: 'utf-8'})
+
+        response.sendStatus(200);
+    })
+
+app.patch('/items/:index',
+    nameNotEmpty().optional(),
+    quantityIntMinOne().optional(),
+    async (request, response) => {
+
+        const validationIssues = validationResult(request);
+        if (!validationIssues.isEmpty()) {
+            response
+                .status(400)
+                .json(validationIssues.array());
+
+            return
+        }
+
+        const data = await promises.readFile(DATA_FILE, {encoding: 'utf8'});
+        const shoppingList = JSON.parse(data);
+        const i = request.params.index;
+
+        if (shoppingList[i]) {
+            shoppingList[i] = {
+                ...shoppingList[i],
+                ...request.body
+            }
+
+            await promises.writeFile(DATA_FILE, JSON.stringify(shoppingList), {encoding: 'utf-8'});
+            response.sendStatus(200);
+        } else {
+            response.sendStatus(404);
+        }
+
+    })
 
 /**
  * Delete a shopping list item from a given position.
  */
 app.delete('/items/:index', async (request, response) => {
-    const data = await promises.readFile(DATA_FILE, {encoding:'utf8'})
+    const data = await promises.readFile(DATA_FILE, {encoding: 'utf8'})
     const shoppingList = JSON.parse(data);
 
     const itemIndex = request.params['index'];
 
     shoppingList.splice(itemIndex, 1)
 
-    await promises.writeFile(DATA_FILE, JSON.stringify(shoppingList), {encoding:'utf-8'})
+    await promises.writeFile(DATA_FILE, JSON.stringify(shoppingList), {encoding: 'utf-8'})
 
     response.sendStatus(200);
 })
 
+app.post(
+    '/items',
+    nameNotEmpty(),
+    quantityIntMinOne(),
+    async (request, response) => {
+
+        const validationIssues = validationResult(request);
+        if (!validationIssues.isEmpty()) {
+            response
+                .status(400)
+                .json(validationIssues.array());
+
+        } else {
+            const data = await promises.readFile(DATA_FILE, {encoding: 'utf8'})
+            const shoppingList = JSON.parse(data)
+
+            const newItem = request.body
+            shoppingList.push(newItem)
+
+            await promises.writeFile(DATA_FILE, JSON.stringify(shoppingList), {encoding: 'utf8'})
+            response.sendStatus(200);
+        }
+
+    })
+
 /**
  * Create the DATA_FILE if it does not exist yet.
  */
-if(!existsSync(DATA_FILE)){
-    await promises.writeFile(DATA_FILE, JSON.stringify([]), {encoding:'utf-8'})
+if (!existsSync(DATA_FILE)) {
+    await promises.writeFile(DATA_FILE, JSON.stringify([]), {encoding: 'utf-8'})
 }
 
 const port = 8000;
---
+++ www/script/data.js
@@ -0,0 +1,16 @@
+
+document.addEventListener("DOMContentLoaded", async (event) => {
+    const response = await fetch('/items');
+    const items = await response.json();
+
+    const shoppingList=document.querySelector('#shoppingList');
+
+    items.forEach((item)=> {
+        let li = document.createElement('li');
+        li.innerText = `${item.quantity} - ${item.name}`;
+        shoppingList.appendChild(li);
+    })
+});
+
+
+
